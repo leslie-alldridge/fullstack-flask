faf22a088cc0486479b15a013f6a2790a6edbf76
leslie-alldridge <leslie.alldridge@gmail.com>
2019-02-12 21:42:39 +1300

    remove old print line

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    print SQLALCHEMY_DATABASE_URI
 
 class TestConfig(BaseConfig):
     DEBUG = True
