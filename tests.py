61a188aecf6a3d2c67feb74a890518c12c5a0d11
leslie-alldridge <leslie.alldridge@gmail.com>
2019-02-12 21:21:42 +1300

    new tests

--- tests.py
+++ tests.py
@@ -1,91 +1,85 @@
-from app import app
 import unittest
+from flask_testing import TestCase
+from project import app, db
+from project.models import User, BlogPost
+
+class BaseTestCase(TestCase):
+    """A base test case."""
+
+    def create_app(self):
+        app.config.from_object('config.TestConfig')
+        return app
+
+    def setUp(self):
+        db.create_all()
+        db.session.add(BlogPost("Test post", "This is a test. Only a test."))
+        db.session.add(User("admin", "ad@min.com", "admin"))
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
 
-class FlaskTestCase(unittest.TestCase):
+class FlaskTestCase(BaseTestCase):
 
     # Ensure that Flask was set up correctly
     def test_index(self):
-        tester = app.test_client(self)
-        response = tester.get('/login', content_type='html/text')
+        response = self.client.get('/login', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-
-    # Ensure welcome page loads
-    def test_welcome(self):
-        tester = app.test_client(self)
-        response = tester.get('/welcome', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-
-    # Ensure logout page redirects to sign in page
-    def test_logout_redirect(self):
-        tester = app.test_client(self)
-        response = tester.get('/logout', content_type='html/text')
-        self.assertEqual(response.status_code, 302)
-
-    # Ensure root page redirects
-    def test_root(self):
-        tester = app.test_client(self)
-        response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 302)
 
     # Ensure that the login page loads correctly
     def test_login_page_loads(self):
-        tester = app.test_client(self)
-        response = tester.get('/login')
+        response = self.client.get('/login')
         self.assertIn(b'Please login', response.data)
 
     # Ensure login behaves correctly with correct credentials
     def test_correct_login(self):
-        tester = app.test_client()
-        response = tester.post(
+
+        response = self.client.post(
             '/login',
             data=dict(username="admin", password="admin"),
             follow_redirects=True
         )
-        self.assertIn(b'You were just logged in!', response.data)
+        self.assertIn(b'You were logged in', response.data)
 
     # Ensure login behaves correctly with incorrect credentials
     def test_incorrect_login(self):
-        tester = app.test_client()
-        response = tester.post(
+        response = self.client.post(
             '/login',
             data=dict(username="wrong", password="wrong"),
             follow_redirects=True
         )
-        self.assertIn(b'Invalid credentials. Please try again.', response.data)
+        self.assertIn(b'Invalid username or password.', response.data)
 
     # Ensure logout behaves correctly
     def test_logout(self):
-        tester = app.test_client()
-        tester.post(
+        self.client.post(
             '/login',
             data=dict(username="admin", password="admin"),
             follow_redirects=True
         )
-        response = tester.get('/logout', follow_redirects=True)
+        response = self.client.get('/logout', follow_redirects=True)
         self.assertIn(b'You were logged out', response.data)
 
     # Ensure that main page requires user login
     def test_main_route_requires_login(self):
-        tester = app.test_client()
-        response = tester.get('/', follow_redirects=True)
+        response = self.client.get('/', follow_redirects=True)
         self.assertIn(b'You need to login first.', response.data)
 
     # Ensure that logout page requires user login
     def test_logout_route_requires_login(self):
-        tester = app.test_client()
-        response = tester.get('/logout', follow_redirects=True)
+        response = self.client.get('/logout', follow_redirects=True)
         self.assertIn(b'You need to login first.', response.data)
 
     # Ensure that posts show up on the main page
     def test_posts_show_up_on_main_page(self):
-        tester = app.test_client()
-        response = tester.post(
+        response = self.client.post(
             '/login',
             data=dict(username="admin", password="admin"),
             follow_redirects=True
         )
-        self.assertIn(b'Good', response.data)
+        self.assertIn(b'This is a test. Only a test.', response.data)
 
 
 if __name__ == '__main__':
