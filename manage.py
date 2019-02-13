e4e7a5722b70482f432edff7fe83f9f9c0e9523c
leslie-alldridge <leslie.alldridge@gmail.com>
2019-02-13 20:37:25 +1300

    coverage tests

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+import unittest
 import os
+import coverage
 
 from project import app, db
 
@@ -9,6 +11,28 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    """runs the tests without coverage"""
+    tests = unittest.Testloader().discover('.')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.command
+def cov():
+    """Runs the unit tests with coverage."""
+    cov = coverage.coverage(branch=True, include='project/*')
+    cov.start()
+    tests = unittest.TestLoader().discover('.')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    cov.stop()
+    cov.save()
+    print 'Coverage Summary:'
+    cov.report()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    covdir = os.path.join(basedir, 'coverage')
+    cov.html_report(directory=covdir)
+    cov.erase()
 
 if __name__ == '__main__':
     manager.run()
