8a60d85ebd010035f1adf00265206c18fe1adafa
leslie-alldridge <leslie.alldridge@gmail.com>
2019-02-13 19:57:09 +1300

    register

--- project/users/views.py
+++ project/users/views.py
@@ -1,8 +1,8 @@
 from flask import flash, redirect, render_template, request, \
     url_for, Blueprint
 from flask_login import login_user, login_required, logout_user
-
-from .form import LoginForm
+from project import db
+from .form import LoginForm, RegisterForm
 from project.models import User, bcrypt
 
 #config
@@ -40,3 +40,18 @@ def logout():
     logout_user()
     flash('You were logged out.')
     return redirect(url_for('home.welcome'))
+
+@users_blueprint.route('/register/', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(
+            name=form.username.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return redirect(url_for('home.home'))
+    return render_template('register.html', form=form)
