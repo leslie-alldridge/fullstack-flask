bba7d835c6d8432cebee852b28d3bdb3dba4a189
leslie-alldridge <leslie.alldridge@gmail.com>
2019-02-13 20:24:32 +1300

    blog form

---
+++ project/home/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import TextField, PasswordField
+from wtforms.validators import DataRequired, Length
+
+class MessageForm(FlaskForm):
+    title = TextField('Title', validators=[DataRequired()])
+    description = TextField('Description', validators=[DataRequired(), Length(max=140)])
--- project/home/views.py
+++ project/home/views.py
@@ -1,7 +1,9 @@
+from flask import render_template, Blueprint, request, flash, redirect, url_for
+from flask_login import login_required, current_user
+
+from .forms import MessageForm
 from project import db
 from project.models import BlogPost
-from flask import render_template, Blueprint
-from flask_login import login_required
 
 #config
 
@@ -10,16 +12,31 @@ home_blueprint = Blueprint(
     template_folder='templates'
 )
 
+
 #routes
 
 # use decorators to link the function to a url
-@home_blueprint.route('/')
+@home_blueprint.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    posts = db.session.query(BlogPost).all()
-    return render_template('index.html', posts=posts)  # render a template
+    error = None
+    form = MessageForm(request.form)
+    if form.validate_on_submit():
+        new_message = BlogPost(
+            form.title.data,
+            form.description.data,
+            current_user.id
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        flash('New entry was successfully posted. Thanks.')
+        return redirect(url_for('home.home'))
+    else:
+        posts = db.session.query(BlogPost).all()
+        return render_template(
+            'index.html', posts=posts, form=form, error=error)
 
 
 @home_blueprint.route('/welcome')
 def welcome():
-    return render_template('welcome.html')  # render a template
+    return render_template('welcome.html')
